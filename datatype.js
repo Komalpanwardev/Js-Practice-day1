64829496fb49ca7f26c748a303fb9ee443ed4d34
Komalpanwardev <komalpanwarsml@gmail.com>
2026-05-18 02:14:35 +0530

    20 practice Question of Data types

---
+++ datatype.js
@@ -0,0 +1,87 @@
+// Question1
+let name = "Komal";
+console.log(typeof name);
+
+// Question2;
+let age = 22;
+console.log(typeof age);
+
+// Question3
+let isLogin = true;
+console.log(typeof isLogin);
+
+// Qustion4
+let city;
+console.log(typeof city);
+
+// Question5
+let value = null;
+console.log(typeof value);
+
+// Question6
+let bigNumber = 123456789123456789n;
+console.log(typeof bigNumber);
+
+// Question7
+let id = Symbol("id");
+console.log(typeof id);
+
+// Question8
+let arr = [1, 2, 3];
+console.log(typeof arr);
+
+// Question9
+let person = {
+  name: "Komal",
+  age: 22,
+};
+
+console.log(typeof person);
+
+// Question10
+function greet() {
+  console.log("Hello");
+}
+
+console.log(typeof greet);
+
+// question11
+console.log(typeof "100");
+
+// Question12
+console.log(typeof 100);
+
+// Question13
+console.log(typeof false);
+
+// question14
+console.log(typeof undefined);
+
+// Question15
+console.log(typeof []);
+
+// Question16
+let num = "50";
+
+console.log(Number(num));
+console.log(typeof Number(num));
+
+// Question17
+let myvalue = 100;
+
+console.log(String(myvalue));
+console.log(typeof String(myvalue));
+
+// question18
+console.log(Boolean(1));
+console.log(Boolean(0));
+
+// Question19
+console.log(Boolean("Komal"));
+console.log(Boolean(""));
+
+// Question20
+let score = "99";
+
+console.log(score + 1);
+console.log(Number(score) + 1);
