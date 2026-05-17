50dfc90bf74346535c78555c5a523fba046954e6
Komalpanwardev <komalpanwarsml@gmail.com>
2026-05-18 01:46:51 +0530

    Practice 20 Question of Variable in Js

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Js-Practice-day1
-Javascript Practice : Variables,Data Types,Operators,and Loops.
+Javascript Practice : Variables,Data Types,Operators,and Loops.<br>
 Today Topic Covered:<br>
 -Variable<br>
 -Data Types<br>
 -Operators<br>
 -Loops<br>
-
+20 Queston practice of Variable
 
--- variable.js
+++ variable.js
@@ -1,1 +1,159 @@
-console.log("komal")
+/*Question1
+Ek variable name banao aur usme apna naam store karo. Console me print karo.*/
+let name = "komal";
+console.log(name);
+
+// Question 2
+// age variable me apni age store karo aur print karo.
+let age = 22;
+console.log(age);
+
+// Question3
+// city variable me apne city ka naam store karo.
+let city = "shamli";
+console.log(city);
+
+// Question4
+// isStudent naam ka boolean variable banao aur true assign karo.
+let isStudent = true;
+console.log(isStudent);
+
+// Question5
+// price variable me 499 store karo aur uska datatype check karo
+let price = 499;
+console.log(typeof price);
+
+// Question6
+// let aur const ka use karke 2 variables banao.
+// let age = 22,
+// const country = "India";
+
+// console.log(age);
+// console.log(country);
+
+// Question7
+// Ek variable me number store karo aur usme 10 add karo.
+let num = 30;
+console.log(num + 10);
+
+// Question8
+// marks variable me 85 store karo aur print karo:
+// "My marks are 85"
+let marks = 85;
+console.log("My marks are", marks);
+
+// Question9
+// firstName aur lastName variables banao aur full name print karo.
+let firstname = "komal";
+let lastname = "panwar";
+console.log(firstname + lastname);
+
+// Question10
+// Ek variable language banao aur usme "JavaScript" store karo.
+let language = "JavaScript";
+console.log(language);
+
+// Queation11
+// a = 10 aur b = 20 store karo. Dono ka sum ek naye variable me store karo.
+let a = 10;
+let b = 20;
+let sum = a + b;
+console.log(sum);
+
+// Question12
+// score variable me value store karo aur usko update karo.
+let score = 50;
+console.log(score);
+score = 90;
+console.log(score);
+
+// Question13
+// const pi = 3.14 banao aur usko change karne ki try karo. Kya error aata hai?
+// const pi = 3.14;
+// console.log(pi);
+// pi = 3.14159;
+// console.log(pi);
+// Error: Assignment to constant variable
+
+// Question14
+//  userName variable banao aur check karo:
+//  typeof userName
+let userName = "Komal";
+
+console.log(typeof userName);
+
+// Question15
+// Ek variable me null store karo aur datatype print karo.
+let data = null;
+console.log(typeof data);
+
+// Question16
+// Ek variable me undefined store karo.
+let value = undefined;
+console.log(value);
+console.log(typeof value);
+
+// Question 17
+// x = 5
+// y = "5"
+// Dono ka datatype compare karo.
+let x = 5;
+let y = "5";
+console.log(typeof x);
+console.log(typeof y);
+
+// question18
+// favNumber variable banao aur usko string me convert karo.
+let favNumber = 100;
+
+let convertedNumber = String(favNumber);
+
+console.log(convertedNumber);
+console.log(typeof convertedNumber);
+
+// 2nd type
+// let favNumber = 10025;
+
+// console.log(favNumber.toString());
+// console.log(typeof favNumber);
+
+// Question 19
+// message variable me:
+// "I am learning JavaScript"
+// store karo aur print karo.
+
+let message = "I am learing Javascript";
+console.log(message);
+
+// question20
+// Ek mini biodata program banao using variables:
+// name
+// age
+// city
+// course
+// output example
+// My name is Komal.
+// I am 22 years old.
+// I live in Delhi.
+// I am learning MERN Stack.
+
+let myname = "Komal";
+let myage = 22;
+let mycity = "Delhi";
+let course = "MERN Stack";
+
+console.log("My name is " + myname);
+console.log("I am " + myage + " years old.");
+console.log("I live in " + mycity);
+console.log("I am learning " + course);
+
+// 2 nd method
+// let name = "Komal";
+// let age = 22;
+// let city = "Delhi";
+// let course = "MERN Stack";
+
+// console.log(`My name is ${name}`);
+// console.log(`I am ${age} years old.`);
+// console.log(`I live in ${city}`);
+// console.log(`I am learning ${course}`);
